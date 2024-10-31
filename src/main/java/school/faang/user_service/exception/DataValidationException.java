927f232959ba7f36301558f98a9c4cb4fe05a983
AlekseyB86 <avbelov86@mail.ru>
2024-10-31 16:00:29 +0300

    delete unused imports

--- src/main/java/school/faang/user_service/exception/DataValidationException.java
+++ src/main/java/school/faang/user_service/exception/DataValidationException.java
@@ -1,7 +1,5 @@
 package school.faang.user_service.exception;
 
-import org.jetbrains.annotations.NotNull;
-
 public class DataValidationException extends RuntimeException {
     public DataValidationException(String message) {
         super(message);
