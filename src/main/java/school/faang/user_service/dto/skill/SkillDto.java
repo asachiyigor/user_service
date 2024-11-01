71381ab682366dd3de574c85aa790e701422983b
Ilya Ishanov <ilia.detroit2016@yandex.ru>
2024-11-02 01:12:30 +0500

    Создание и настройка скиллов

--- src/main/java/school/faang/user_service/dto/skill/SkillDto.java
+++ src/main/java/school/faang/user_service/dto/skill/SkillDto.java
@@ -1,10 +1,14 @@
 package school.faang.user_service.dto.skill;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@NotNull
+@NotEmpty
 public class SkillDto {
     private Long id;
     private String title;
