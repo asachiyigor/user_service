7f55888096b8e79ff9c903a38473096b0d492af2
AlekseyB86 <avbelov86@mail.ru>
2024-11-02 20:05:39 +0300

    удалил импорт

--- src/main/java/school/faang/user_service/entity/recommendation/SkillRequest.java
+++ src/main/java/school/faang/user_service/entity/recommendation/SkillRequest.java
@@ -2,7 +2,6 @@ package school.faang.user_service.entity.recommendation;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import school.faang.user_service.entity.Skill;
