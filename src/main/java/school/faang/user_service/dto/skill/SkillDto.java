ff4010b68cd2fbbfd4422da0d5320c4abfe1bc13
Aleksey <aleksei.zuk86@gmail.com>
2024-11-09 13:51:45 +0300

    чет с конфликтами

--- src/main/java/school/faang/user_service/dto/skill/SkillDto.java
+++ src/main/java/school/faang/user_service/dto/skill/SkillDto.java
@@ -3,10 +3,12 @@ package school.faang.user_service.dto.skill;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class SkillDto {
