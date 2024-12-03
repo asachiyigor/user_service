eadc878e404754602b9c21c2415b9b8811c61c23
Maksim <15maksim@mail.ru>
2024-12-03 12:57:29 +0300

    Update RedisProperties.java

--- src/main/java/school/faang/user_service/config/context/redis/RedisProperties.java
+++ src/main/java/school/faang/user_service/config/context/redis/RedisProperties.java
@@ -1,16 +1,8 @@
 package school.faang.user_service.config.context.redis;
 
-import lombok.Getter;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@Component
-@Getter
-public class RedisProperties {
-    @Value("${spring.data.redis.host}")
-    private String host;
-    @Value("${spring.data.redis.port}")
-    private Integer port;
-    @Value("${spring.data.redis.user-ban-topic}")
-    private String userBanTopic;
+
+@ConfigurationProperties(prefix = "spring.data.redis")
+public record RedisProperties(Integer port, String host, String usersBanTopic) {
 }
